b707703f9a420c881c9a22e5445e252f2633ded1
Peter Schroeter <peter.schroeter@rightscale.com>
2012-05-23 22:07:00 -0700

    vim changes

---
+++ .vim/syntax/python.vim
@@ -0,0 +1,3 @@
+" highlight tabs in alternate color
+syntax match Tab /\t/
+hi Tab gui=underline guifg=blue ctermbg=blue
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-git pull
+dotdir="$(dirname "$0")"
+thisdate=$(date +%Y%m%d%M%S)
+cd $dotdir
+dotdir=$(pwd -P)
+
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+#  mkdir -p ._backup/$thisdate
+  for f in $(ls -a .); do 
+    [ "$f" = "." ] && continue
+    [ "$f" = ".." ] && continue
+    [ "$f" = ".osx" ] && continue
+    [ "$f" = ".brew" ] && continue
+    [ "$f" = ".git" ] && continue
+    [ "$f" = "._backup" ] && continue
+    [ "$f" = ".vim" ] && continue
+    [[ ! "$f" =~ ^\. ]] && continue
+#    mv $HOME/$f $dotdir/._backup/$thisdate
+#    ln -s $dotdir/$f $HOME/$f
+    echo "ln -s $dotdir/$f $HOME/$f"
+  done
+#  mv $HOME/.vim $dotdir/._backup/$thisdate
+#  mkdir -p $HOME/.vim/backups
+#  mkdir -p $HOME/.vim/undo
+#  mkdir -p $HOME/.vim/swaps
+  for f in $(ls .vim); do 
+    [ "$f" = "backups" ] && continue
+    [ "$f" = "undo" ] && continue
+    [ "$f" = "swaps" ] && continue
+#    ln -s $dotdir/.vim/$f $HOME/.vim/$f
+    echo "ln -s $dotdir/.vim/$f $HOME/.vim/$f"
+  done
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+  doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
 fi
 unset doIt
 source ~/.bash_profile
